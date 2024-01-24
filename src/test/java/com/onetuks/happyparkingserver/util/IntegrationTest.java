746e2633411c576d843c0a60466156a3ece9c274
onetuks <onetuks@naver.com>
2024-01-25 01:03:49 +0900

    Chore: TestContainers 설정

--- src/test/java/com/onetuks/happyparkingserver/util/IntegrationTest.java
+++ src/test/java/com/onetuks/happyparkingserver/util/IntegrationTest.java
@@ -1,9 +1,14 @@
 package com.onetuks.happyparkingserver.util;
 
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @ActiveProfiles(value = "test")
+@Testcontainers
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class IntegrationTest {
 
